264291553f6e7d740ad95603162cfbd08fd8c63d
Sammy-T <cyam9019@aim.com>
2023-10-27 02:06:50 -0400

    Path confusion?

--- svelte/cms/src/lib/stores.js
+++ svelte/cms/src/lib/stores.js
@@ -27,7 +27,11 @@ export const cmsActions = derived(config, ($config, set) => {
 });
 
 async function loadConfig() {
-    //// TODO: Why is this path different?
+    /**
+     * I'm not sure why this path is different (`../` instead of `../../`)
+     * but I'm guessing Vite handles it differently
+     * because the url string argument is static instead of built at runtime.
+     */
     const configUrl = new URL('../cms-config/config.yml', import.meta.url).href;
 
     const res = await fetch(configUrl);
