b82eb7f0b9b05327982692c0548c7fbf71c5312d
Sammy-T <cyam9019@aim.com>
2024-06-04 20:25:38 -0400

    Output to 'dist' directory

--- svelte/cms/vite.config.js
+++ svelte/cms/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
         }
     },
     build: {
-        outDir: '../../site/static/cms/',
+        outDir: '../../dist/cms/',
         emptyOutDir: true,
         rollupOptions: {
             output: {
