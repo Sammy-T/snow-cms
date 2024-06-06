73c113f9d889035afe56a1024e8e35469e566f72
Sammy-T <cyam9019@aim.com>
2024-06-05 21:02:02 -0400

    Remove explicit 'global' definition
    see: https://github.com/pouchdb/pouchdb/issues/8516

--- cms/src/main.js
+++ cms/src/main.js
@@ -1,8 +1,6 @@
 import './main.css';
 import App from './lib/App.svelte';
 
-//// TODO: Set `window.global` here instead of from the html?
-
 const app = new App({
     target: document.getElementById('app'),
 });
