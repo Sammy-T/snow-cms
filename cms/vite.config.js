f45236e6ce152ac119e1a0a3650e5c38b4ce0e41
Sammy-T <cyam9019@aim.com>
2024-06-05 20:26:41 -0400

    Disable module preloading
    Preloading can cause issues with some of the vendor modules.

--- cms/vite.config.js
+++ cms/vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
     build: {
         outDir: '../dist/cms',
         emptyOutDir: true,
+        modulePreload: false,
         rollupOptions: {
             output: {
                 entryFileNames: 'assets/[name].js',
