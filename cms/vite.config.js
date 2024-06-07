a3bbc4edc8cf80f85895594df00e967a1e30cb08
Sammy-T <cyam9019@aim.com>
2024-06-06 20:26:46 -0400

    Move manual chunking terms to constant variable

--- cms/vite.config.js
+++ cms/vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path'
 
+/** The terms used to separate vendor modules into separate chunks. */
+const vendorModules = [
+    '@milkdown', 'dayjs', 'js-yaml', 'micromark', 'pouchdb', 
+    'prosemirror', 'svelte'
+];
+
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '',
@@ -19,6 +25,7 @@ export default defineConfig({
         outDir: '../dist/cms',
         emptyOutDir: true,
         modulePreload: false,
+        // sourcemap: true,
         rollupOptions: {
             output: {
                 entryFileNames: 'assets/[name].js',
@@ -28,10 +35,7 @@ export default defineConfig({
                     if (id.includes('node_modules')) {
                         let name = 'vendor';
 
-                        [
-                            '@milkdown', 'dayjs', 'js-yaml', 'micromark', 'pouchdb', 
-                            'prosemirror', 'svelte'
-                        ].forEach(module => {
+                        vendorModules.forEach(module => {
                             if(id.includes(module)) name = `vendor-${module}`;
                         });
 
